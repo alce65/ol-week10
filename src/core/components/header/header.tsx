5bf793a63699316199083851bf5933e392b0332d
Alejandro Cerezo <alce65@hotmail.es>
2023-01-13 22:41:24 +0100

    Consume context in header component

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
+import { NoteContext } from '../../context/note.context';
 import './header.css';
 
 export function Header({ children }: { children: JSX.Element }) {
     const title = 'Learning Components';
+    const { notes } = useContext(NoteContext);
 
     return (
         <header aria-label="title">
             <h1>{title}</h1>
+            <p className="notas-number">Notas: {notes.length}</p>
             {children}
         </header>
     );
