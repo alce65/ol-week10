cb842fea02f7d97319d2cdfced236034501d0830
Alejandro Cerezo <alce65@hotmail.es>
2023-01-14 12:16:39 +0100

    Add component List in PlacesPage

--- src/features/places/pages/places.page.tsx
+++ src/features/places/pages/places.page.tsx
@@ -1,10 +1,10 @@
-// NEXT import { List } from '../components/list/list';
+import { List } from '../components/list/list';
 
 export default function PlacesPage() {
     return (
         <>
             <h2>Places Page</h2>
-            {/* <List></List> */}
+            <List></List>
         </>
     );
 }
